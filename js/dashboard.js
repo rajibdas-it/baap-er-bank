25dfe353674ebc84d226b9e08e78f11d716aeae9
Rajib Das <rajibrad@gmail.com>
2022-08-16 12:01:05 +0600

    withdraw function complete

--- js/dashboard.js
+++ js/dashboard.js
@@ -15,3 +15,21 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   const totalAmount = currentTotalAmount + newDepositAmount;
   existingTotalAmount.innerText = totalAmount;
 });
+
+document.getElementById("btn-withdraw").addEventListener("click", function () {
+  const currentWithdrawAmt = document.getElementById("current-withdraw");
+  const currnetAmountStirng = currentWithdrawAmt.innerText;
+  const currentAmount = parseFloat(currnetAmountStirng);
+
+  const newWithdraw = document.getElementById("withdraw-field");
+  const newWithdrawString = newWithdraw.value;
+  const newWithdrawAmount = parseFloat(newWithdrawString);
+  const totalWithdrawAmount = currentAmount + newWithdrawAmount;
+  currentWithdrawAmt.innerText = totalWithdrawAmount;
+  newWithdraw.value = "";
+  const existingTotalAmount = document.getElementById("current-amount");
+  const totalAmountString = existingTotalAmount.innerText;
+  const currentTotalAmount = parseFloat(totalAmountString);
+  const totalAmount = currentTotalAmount - newWithdrawAmount;
+  existingTotalAmount.innerText = totalAmount;
+});
